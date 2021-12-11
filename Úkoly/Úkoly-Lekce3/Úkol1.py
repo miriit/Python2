014cf38774b869ba4f064d2e8b2fc7a9e5c3b324
miriit <Miriam9879>
2021-12-11 19:46:31 +0100

    Úkol1

--- Úkoly/Úkoly-Lekce3/Úkol1.py
+++ Úkoly/Úkoly-Lekce3/Úkol1.py
@@ -1,11 +1,5 @@
-import numpy
 import pandas
 import requests
-
-desired_width = 1000
-pandas.set_option('display.max_columns', None)
-pandas.set_option("display.width", desired_width)
-numpy.set_printoptions(linewidth=desired_width)
 
 r = requests.get("https://raw.githubusercontent.com/lutydlitatova/czechitas-datasets/main/datasets/lexikon-zvirat.csv")
 open("lexikon-zvirat.csv", "wb").write(r.content)
@@ -14,19 +8,20 @@ zvirata = pandas.read_csv("lexikon-zvirat.csv", sep=";")
 zvirata = zvirata.dropna(how="all", axis="rows")
 zvirata = zvirata.dropna(how="all", axis="columns")
 zvirata = zvirata.set_index("id")
-print(zvirata)
-"""
+#print(zvirata)
+
 def check_url(radek):
-    if isinstance(radek.image_src, str):
-        if radek.image_src.startswith("https://zoopraha.cz/images/"):
+     if isinstance(radek.image_src, str):
+         if radek.image_src.startswith("https://zoopraha.cz/images/"):
             if radek.image_src.endswith(".jpg"):
-                return radek.title
-
-for radek in zvirata.itertuples():
-    print(check_url(zvirata))"""
-#radek.image_src.startswith("https://zoopraha.cz/images/")
-
-
+                print({radek.image_src})
+            else:
+                print(radek.title)
 
 #for zvire in zvirata.itertuples():
-#    check_url()
+#    check_url(zvire)
+
+for idx, zvire in zvirata.iterrows():
+    check_url(zvire)
+
+
